b6c7846f67168f06bf985350dd151d971f6ee02d
BaraaAlAli553 <baraaalai553@gmail.com>
2026-08-17 18:25:05 +0300

    feat: enhance student complaints management

--- lib/core/api/api_links.dart
+++ lib/core/api/api_links.dart
@@ -1,9 +1,9 @@
 class ApiLinks {
   //karam link
- // static const String baseUrl = 'http://192.168.1.108:8000/api';
+  static const String baseUrl = 'http://192.168.1.108:8000/api';
 
   //baraa link0
-   static const String baseUrl = 'http://192.168.50.2:8001/api';
+   //static const String baseUrl = 'http://192.168.50.2:8001/api';
 
   ////////authentication
   static const String register = '$baseUrl/register';
